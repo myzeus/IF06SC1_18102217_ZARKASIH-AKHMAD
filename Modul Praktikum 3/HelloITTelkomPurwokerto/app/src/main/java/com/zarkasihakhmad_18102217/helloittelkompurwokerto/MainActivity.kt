372af9a7a5885ed855821ed7973f8df1ff6ea642
myzeus <zeusselalusalah@gmail.com>
2020-10-24 20:17:16 +0700

    Hello world Prioject

---
+++ Modul Praktikum 3/HelloITTelkomPurwokerto/app/src/main/java/com/zarkasihakhmad_18102217/helloittelkompurwokerto/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.zarkasihakhmad_18102217.helloittelkompurwokerto
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
