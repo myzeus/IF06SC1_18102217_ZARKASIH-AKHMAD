dade2c04705c4639d640d20734a0a4362db685ba
myzeus <zeusselalusalah@gmail.com>
2021-01-15 16:03:19 +0700

    initial commit praktikum 11 + step tambahan
    commit tugas

---
+++ Modul Praktikum 10/app/build/generated/source/buildConfig/debug/com/zarkasihakhmad_18102217/praktikum10/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.zarkasihakhmad_18102217.praktikum10;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.zarkasihakhmad_18102217.praktikum10";
+  public static final String BUILD_TYPE = "debug";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+}
